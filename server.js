52b20e4c1faf3af66ee5efae5a765823322b911b
Liam Peachey <liamjpeachey@gmail.com>
2023-11-01 01:14:57 -0500

    Filled in server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('.config/connection');
+const SequelizeStore = require('connect-session-sequelize');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Give handlebar files access to the functions created in utils/helper.json
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'tell no one',
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict'
+    },
+    resave: false,
+    saveUninitiated: true,
+    store: new SequelizeStore({ db: sequelize })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view-engine', 'handlebars');
+
+app.use(express.json());
+app.listen(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
+});
