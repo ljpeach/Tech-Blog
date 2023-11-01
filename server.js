525142e537eb3107d015e8e501b1d120143f82cd
Liam Peachey <liamjpeachey@gmail.com>
2023-11-01 07:43:35 -0500

    Fixed some bugs in server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-const sequelize = require('.config/connection');
-const SequelizeStore = require('connect-session-sequelize');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,7 +22,7 @@ const sess = {
         sameSite: 'strict'
     },
     resave: false,
-    saveUninitiated: true,
+    saveUninitialized: true,
     store: new SequelizeStore({ db: sequelize })
 };
 
@@ -32,7 +32,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view-engine', 'handlebars');
 
 app.use(express.json());
-app.listen(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
